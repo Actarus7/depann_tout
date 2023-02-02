85bd9efa3eef33b56e34f843cbca8087b449a779
BATARD Kevin (SNCF RESEAU / EIC AQUITAINE POITOU-CHARENTES / CIRC BAYONNE) <9503305e@commun.ad.sncf.fr>
2023-02-02 10:38:57 +0100

    feat création service entity

--- src/services/entities/service.entity.ts
+++ src/services/entities/service.entity.ts
@@ -1,1 +1,25 @@
-export class Service {}
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Service extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'character varying' })
+  name: string;
+
+  @Column({ type: 'numeric' })
+  price: number;
+
+  @Column({ type: 'character varying' })
+  city: string;
+
+  @Column({ type: 'timestamp with local time zone' })
+  start_time: Date;
+
+  @Column({ type: 'timestamp with local time zone' })
+  end_time: Date;
+
+  @Column({ type: 'boolean', default: false })
+  reserved: boolean;
+}
