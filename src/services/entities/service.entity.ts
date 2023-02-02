4db704a2757a7976a094e4d25ee9f79cafa6d2ef
Steff <steff.1@gmail.com>
2023-02-02 13:59:54 +0100

    feat add création user avec hashage + login - fix mineurs

--- src/services/entities/service.entity.ts
+++ src/services/entities/service.entity.ts
@@ -21,10 +21,10 @@ export class Service extends BaseEntity {
   @Column({ type: 'character varying' })
   city: string;
 
-  @Column({ type: 'timestamp with local time zone' })
+  @Column({ type: 'timestamp' })
   start_time: Date;
 
-  @Column({ type: 'timestamp with local time zone' })
+  @Column({ type: 'timestamp' })
   end_time: Date;
 
   @Column({ type: 'boolean', default: false })
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,75 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ConflictException, Res, BadRequestException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Response } from 'express';
+
+import * as bcrypt from 'bcrypt';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
+  async create(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
+
+    // VERIFIE SI LE USERNAME EST DEJA EXISTANT
+    const saltOrRounds = 10;
+    
+    const isUsernameExists = await this.usersService.findOneByUsername(createUserDto.username);
+
+    if (isUsernameExists) {
+      throw new ConflictException('Username déjà existant, choisissez-en un autre');
+    };
+
+
+    // VERIFIE SI L'EMAIL EST DEJA EXISTANT
+    const isEmailExists = await this.usersService.findOneByEmail(createUserDto.e_mail);
+
+    if (isEmailExists) {
+      throw new ConflictException('Username déjà existant, choisissez-en un autre');
+    };
+
+
+    // VERIFIE LE PASSWORD ET LA CONFIRMATION PASSWORD
+    if (createUserDto.password !== createUserDto.confirm_password) {
+      throw new BadRequestException('Le password et la confirmation password ne sont pas identiques');
+    };
+
+
+    // HASHAGE DU PASSWORD
+    const hash = await bcrypt.hash(createUserDto.password, saltOrRounds);
+
+    
+    // CREATION D'UN NOUVEAU USER
+    const newUser = await this.usersService.create(createUserDto, hash);
+
+    return res.status(201).json({
+      status: 'OK',
+      message: 'User créé',
+      data: newUser
+    });
+
+  };
 
   @Get()
   findAll() {
     return this.usersService.findAll();
-  }
+  };
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
-  }
+  };
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
-  }
+  };
 
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
-  }
-}
+  };
+
+};
