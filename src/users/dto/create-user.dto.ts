cb6caec82af7a0c0ae6a8b338316cfa100628a56
Steff <steff.1@gmail.com>
2023-02-02 11:19:40 +0100

    feat add exclue password + create dto users

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,31 @@
-export class CreateUserDto {}
+import { IsEmail, IsOptional, IsString } from "class-validator";
+
+export class CreateUserDto {
+
+    @IsString()
+    username: string;
+
+    @IsEmail()
+    e_mail: string;
+
+    @IsString()
+    password: string;
+
+    @IsString()
+    adress_line1: string;
+    
+    @IsString()
+    @IsOptional()
+    adress_line2: string;
+    
+    @IsString()
+    @IsOptional()
+    adress_line3: string;
+
+    @IsString()
+    zipCode: string;
+    
+    @IsString()
+    city: string;
+
+};
