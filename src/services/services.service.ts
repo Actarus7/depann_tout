d8899e11ed0288afa23a87ac1364a946c32012f7
BATARD Kevin (SNCF RESEAU / EIC AQUITAINE POITOU-CHARENTES / CIRC BAYONNE) <9503305e@commun.ad.sncf.fr>
2023-02-02 18:39:58 +0100

    feat recherche de service par nom controller, service

--- src/services/services.service.ts
+++ src/services/services.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { Like } from 'typeorm';
 import { CreateServiceDto } from './dto/create-service.dto';
+import { GetServiceDto } from './dto/get-service.dto';
 import { UpdateServiceDto } from './dto/update-service.dto';
 import { Service } from './entities/service.entity';
 
@@ -25,8 +27,15 @@ export class ServicesService {
     return `This action returns all services`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} service`;
+  async findName(getServiceDto: GetServiceDto) {
+    const findServices = await Service.findBy({
+      name: Like(`%${getServiceDto.name}%`),
+    });
+
+    if (findServices) {
+      return findServices;
+    }
+    return undefined;
   }
 
   async update(userIdLogged: number, updateServiceDto: UpdateServiceDto) {
