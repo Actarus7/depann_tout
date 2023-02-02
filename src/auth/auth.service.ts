736c0deb62f358dce3dae42588cb5d8ef4f93b3d
Steff <steff.1@gmail.com>
2023-02-02 10:49:59 +0100

    feat add users/reservations entities + auth resource

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,36 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly jwtService: JwtService) { }
+
+    // async validateUser(username: string, pass: string): Promise<any> {
+    //     const user = await this.usersService.findOneByUsernameWithPassword(username);
+
+    //     const isMatch = await bcrypt.compare(pass, user.password);
+
+    //     if (isMatch) {
+    //         const { password, ...result } = user;
+    //         return result;
+    //     };
+
+    //     throw new ForbiddenException('Password incorrect');
+    // };
+
+    // async login(/* loginUserDto: any */user: any) {
+    //     // console.log(user);
+
+    //     const payload = { /* username: loginUserDto.username, sub: loginUserDto.id, admin: loginUserDto.admin */username: user.username, sub: user.id, admin: user.admin };
+    //     // console.log(payload);
+
+
+    //     return { id: payload.sub, username: payload.username, access_token: this.jwtService.sign(payload), admin: payload.admin }
+    // };
+
+};
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,38 @@
-export class User {}
+import { Reservation } from "src/reservations/entities/reservation.entity";
+import { Service } from "src/services/entities/service.entity";
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('users')
+export class User extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'varchar' })
+    username: string;
+
+    @Column({ type: 'varchar' })
+    password: string;
+
+    @Column({ type: 'varchar' })
+    adress_line1: string
+
+    @Column({ type: 'varchar' })
+    adress_line2: string
+
+    @Column({ type: 'varchar' })
+    adress_line3: string
+
+    @Column({ type: 'varchar' })
+    zipCode: string
+
+    @Column({ type: 'varchar' })
+    city: string
+
+    @OneToMany(() => Service, service => service.user)
+    services: Service[];
+
+    @OneToMany(() => Reservation, reservation => reservation.user)
+    reservations: Reservation[];
+
+};
