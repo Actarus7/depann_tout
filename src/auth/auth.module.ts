c90fa232983dc12013ee45eddf3784ec4d552cf8
BATARD Kevin (SNCF RESEAU / EIC AQUITAINE POITOU-CHARENTES / CIRC BAYONNE) <9503305e@commun.ad.sncf.fr>
2023-02-03 11:57:18 +0100

    delete constants

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,7 +4,6 @@ import { PassportModule } from '@nestjs/passport/dist';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from 'src/auth/auth.controller';
 import { AuthService } from './auth.service';
-import { jwtConstants } from './constants';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 import { ConfigModule } from '@nestjs/config';
